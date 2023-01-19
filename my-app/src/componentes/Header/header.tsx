a28173ce252f85495b6f3c6882fb02381b5c707f
gabrielmijoler <gabrielmijoler@hotmail.com>
2023-01-19 00:05:19 -0300

    style: componente Header

--- my-app/src/componentes/Header/header.tsx
+++ my-app/src/componentes/Header/header.tsx
@@ -1,5 +1,7 @@
 import React from 'react';
+import { ControlHeader,  Titulo,  Weekeplanner } from '.';
 import logouol from "../Image/logouol.svg"
+import logout from "../Image/logout.svg"
 
 
 const Header: React.FC = (props:any) => {
@@ -21,18 +23,22 @@ let horas = dataAtual.toTimeString()
 // return `${year}${separator}${month<10?`0${month}`:`${month}`}${separator}${date}`
   return (
     <>
-        <section >
-            <section></section>
+        <ControlHeader>
+            <Weekeplanner>
+              <p>
+                <Titulo>Weekly Planner</Titulo><br />
+                Use this planner to organize your daily issues</p ></Weekeplanner>
             <section style={{display:'flex', justifyContent:'center', flexDirection:'column'}}>
                 <p style={{display:'flex', justifyContent:'center'}}>{horas}</p>
                 <p style={{display:'flex', justifyContent:'center'}}>{new Date().toLocaleString("en-US", { month: "long" })}</p>
             </section>
-            <section></section>
-            <section>
-                <img src="" alt="" />
-                <input type="button" src={logouol} />
+            <section>asdadadaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa
             </section>
-        </section>
+            <section>
+                <img src={logouol} alt="Logouol" style={{width: '100px'}} />
+                <input type="button" src={logout}/>
+            </section>
+        </ControlHeader>
         
     </>
     )
---
+++ my-app/src/componentes/Header/index.ts
@@ -0,0 +1,30 @@
+import styled from "styled-components";
+
+
+export const Weekeplanner = styled.section`
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+    padding: 12px 0px 12px 24px;
+    gap: 8px;
+    width: 588px;
+    height: 64px;
+    background: #000000;
+    border-radius: 0px 14px 15px 0px;
+    color: #fff;
+`;
+
+
+export const ControlHeader = styled.section`
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    width: 100%;
+    height: 145px;
+    background: #FFFFFF;
+    box-shadow: 0px 4px 24px rgba(168, 168, 168, 0.25);
+
+`;
+export const Titulo = styled.span`
+    font-size: 20px;
+`;
